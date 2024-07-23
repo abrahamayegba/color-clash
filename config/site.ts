04eebcf8dc0c908915022af4dda12777d4f614b3
Abraham Ayegba <abraham@Abrahams-MacBook-Pro.local>
2024-07-23 18:46:56 +0100

    added fix

--- config/site.ts
+++ config/site.ts
@@ -2,10 +2,10 @@ export const siteConfig = {
   name: "Color Clash",
   author: "Abraham Ayegba",
   description: `A color-matching game that tests and improves your cognitive abilities by leveraging the Stroop effect.`,
-  url: "https://colorclash.vercel.app/",
+  url: "https://color-clash.vercel.app/",
   links: {
     twitter: "https://twitter.com/abrahamayegba_",
-    github: "https://github.com/abrahamayegba/colorclash",
+    github: "https://github.com/abrahamayegba/color-clash",
   },
 };
 
